080e7c7fc524725f6d2ce1c2a41bdefb501e06f8
Mathew Topper <damm_horse@yahoo.co.uk>
2024-12-11 17:25:43 +0000

    Get all tests working again

--- packages/polite/tests/test_paths_directorymap.py
+++ packages/polite/tests/test_paths_directorymap.py
@@ -5,120 +5,110 @@ Created on Thu Mar 31 15:24:33 2016
 @author: Mathew Topper
 """
 
-#pylint: disable=C0103,C0111
+# pylint: disable=C0103,C0111
 
 import glob
 
-from polite.paths import Directory, DirectoryMap
+from polite.paths import DirectoryMap
 
 
-def test_DirectoryMap_copy_all(tmpdir):
-
+def test_DirectoryMap_copy_all(tmp_path):
     # Make a source directory with some files
-    src_tmpdir = tmpdir.mkdir("test_src")
+    src_tmp_path = tmp_path / "test_src"
+    src_tmp_path.mkdir()
 
-    config1 = src_tmpdir.join("config.txt")
-    config1.write("content")
+    config1 = src_tmp_path / "config.txt"
+    config1.write_text("content")
 
-    config2 = src_tmpdir.join("config.yaml")
-    config2.write("content")
-
-    src_dir = Directory(str(src_tmpdir))
+    config2 = src_tmp_path / "config.yaml"
+    config2.write_text("content")
 
     # Make a target directory
-    dst_tmpdir = tmpdir.mkdir("test_dst")
-    dst_dir = Directory(str(dst_tmpdir))
+    dst_tmp_path = tmp_path / "test_dst"
 
-    test_dirmap = DirectoryMap(dst_dir, src_dir)
+    test_dirmap = DirectoryMap(dst_tmp_path, src_tmp_path)
 
     # Test the method in vanilla mode
     test_dirmap.copy_all()
 
-    assert len(dst_tmpdir.listdir()) == 2
+    assert len(list(dst_tmp_path.iterdir())) == 2
 
 
-def test_DirectoryMap_copy_all_new(tmpdir):
-
+def test_DirectoryMap_copy_all_new(tmp_path):
     # Make a source directory with some files
-    src_tmpdir = tmpdir.mkdir("test_src")
+    src_tmp_path = tmp_path / "test_src"
+    src_tmp_path.mkdir()
 
-    config1 = src_tmpdir.join("config.txt")
-    config1.write("content")
+    config1 = src_tmp_path / "config.txt"
+    config1.write_text("content")
 
-    config2 = src_tmpdir.join("config.yaml")
-    config2.write("content")
-
-    src_dir = Directory(str(src_tmpdir))
+    config2 = src_tmp_path / "config.yaml"
+    config2.write_text("content")
 
     # Make a target directory
-    dst_tmpdir = tmpdir.mkdir("test_dst")
-    dst_dir = Directory(str(dst_tmpdir))
+    dst_tmp_path = tmp_path / "test_dst"
 
-    test_dirmap = DirectoryMap(dst_dir, src_dir)
+    test_dirmap = DirectoryMap(dst_tmp_path, src_tmp_path)
 
     # Test the method is creating .new files
     test_dirmap.copy_all()
     test_dirmap.copy_all()
 
-    assert len(dst_tmpdir.listdir()) == 4
-    assert len(glob.glob("*.new", root_dir=str(dst_tmpdir))) == 2
+    assert len(list(dst_tmp_path.iterdir())) == 4
+    assert len(glob.glob("*.new", root_dir=str(dst_tmp_path))) == 2
 
 
-def test_DirectoryMap_copy_all_overwrite(tmpdir):
-
+def test_DirectoryMap_copy_all_overwrite(tmp_path):
     # Make a source directory with some files
-    src_tmpdir = tmpdir.mkdir("test_src")
+    src_tmp_path = tmp_path / "test_src"
+    src_tmp_path.mkdir()
 
-    config1 = src_tmpdir.join("config.txt")
-    config1.write("content")
+    config1 = src_tmp_path / "config.txt"
+    config1.write_text("content")
 
-    config2 = src_tmpdir.join("config.yaml")
-    config2.write("content")
-
-    src_dir = Directory(str(src_tmpdir))
+    config2 = src_tmp_path / "config.yaml"
+    config2.write_text("content")
 
     # Make a target directory
-    dst_tmpdir = tmpdir.mkdir("test_dst")
-    dst_dir = Directory(str(dst_tmpdir))
+    dst_tmp_path = tmp_path / "test_dst"
+    dst_tmp_path.mkdir()
 
     # Make a dst file to be overwritten
-    config3 = dst_tmpdir.join("config.yaml")
-    config3.write("deleteme")
+    config3 = dst_tmp_path / "config.yaml"
+    config3.write_text("deleteme")
 
-    test_dirmap = DirectoryMap(dst_dir, src_dir)
+    test_dirmap = DirectoryMap(dst_tmp_path, src_tmp_path)
 
     # Test the method can overwrite existing files
     test_dirmap.copy_all(overwrite=True)
 
-    assert len(dst_tmpdir.listdir()) == 2
-    assert config3.read() == "content"
+    assert len(list(dst_tmp_path.iterdir())) == 2
+    assert config3.read_text() == "content"
 
 
-def test_DirectoryMap_safe_copy_all(tmpdir):
-
+def test_DirectoryMap_safe_copy_all(tmp_path):
     # Make a source directory with some files
-    src_tmpdir = tmpdir.mkdir("test_src")
+    src_tmp_path = tmp_path / "test_src"
+    src_tmp_path.mkdir()
 
-    config1 = src_tmpdir.join("config.txt")
-    config1.write("content")
+    config1 = src_tmp_path / "config.txt"
+    config1.write_text("content")
 
-    config2 = src_tmpdir.join("config.yaml")
-    config2.write("content")
-
-    src_dir = Directory(str(src_tmpdir))
+    config2 = src_tmp_path / "config.yaml"
+    config2.write_text("content")
 
     # Make a target directory
-    dst_tmpdir = tmpdir.mkdir("test_dst")
-    dst_dir = Directory(str(dst_tmpdir))
+    dst_tmp_path = tmp_path / "test_dst"
+    dst_tmp_path.mkdir()
 
     # Make a dst file not to be overwritten
-    config3 = dst_tmpdir.join("config.yaml")
-    config3.write("deleteme")
+    config3 = dst_tmp_path / "config.yaml"
+    config3.write_text("deleteme")
 
-    test_dirmap = DirectoryMap(dst_dir, src_dir)
+    test_dirmap = DirectoryMap(dst_tmp_path, src_tmp_path)
 
     # Test the method will ovoid overwrite existing files
     test_dirmap.safe_copy_all()
 
-    assert len(dst_tmpdir.listdir()) == 2
-    assert config3.read() == "deleteme"
+    assert len(list(dst_tmp_path.iterdir())) == 2
+    assert config3.read_text() == "deleteme"
