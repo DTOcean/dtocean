a8133e272378bd26791895fa4e42953c076d0bd6
Mathew Topper <damm_horse@yahoo.co.uk>
2024-12-12 13:03:24 +0000

    Rename aneris to mdo-engine and set to version 1

---
+++ packages/mdo-engine/tests/test_utilities_plugins.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""py.test tests on utilities.plugins module
+
+.. moduleauthor:: Mathew Topper <mathew.topper@tecnalia.com>
+"""
+
+import os
+import sys
+import pathlib
+
+import pytest
+
+from polite_config.paths import module_path
+from mdo_engine.utilities.plugins import (get_module_names_from_package,
+                                          get_module_names_from_paths,
+                                          get_class_descriptions_from_module,
+                                          get_subclass_names_from_module,
+                                          get_class_attr)
+
+
+def test_get_module_names_from_package():
+
+    '''Test the modules in the package are as expected'''
+
+    # this is the package we are inspecting -- 'logging' from stdlib
+    import logging
+
+    names = get_module_names_from_package(logging)
+
+    assert names == ['logging.config', 'logging.handlers']
+
+
+def test_get_module_names_from_paths():
+    
+    '''Test if we can retrieve the root package name from the source code'''
+    
+    # Get this module path and then the directory above it.
+    this_mod = sys.modules[__name__]
+    mod_path = module_path(this_mod)
+    root_dir = pathlib.Path(mod_path).parents[1]
+    names = get_module_names_from_paths([root_dir])
+    
+    # Get the expected names by looking for py files in the root_dir
+    expected = []
+    
+    for check_file in os.listdir(root_dir):
+        if check_file.endswith(".py"):
+            name, _ = os.path.splitext(check_file)
+            expected.append(name)
+    
+    assert set(names) >= set(expected)
+
+
+def test_get_class_descriptions_from_module():
+    '''Test if the SeriesData class can be recovered'''
+    mods = get_class_descriptions_from_module('mdo_engine.test.data.definitions')
+    assert 'SeriesData' in mods
+
+
+def test_get_subclass_names_from_module():
+    '''Test if the the SeriesData class is a subclass of the Data class.'''
+    data_sub_mods = get_subclass_names_from_module(
+                                            'mdo_engine.test.data.definitions',
+                                            'Structure')
+    assert 'SeriesData' in data_sub_mods
+
+
+def test_get_class_attr_warn(monkeypatch):
+    
+    def mockerror(path):
+        raise ImportError
+
+    monkeypatch.setattr("importlib.import_module", mockerror)
+    
+    result = get_class_attr("test", "notamodule", True)
+    
+    assert result is None
+    
+def test_get_class_attr_exception(monkeypatch):
+    
+    def mockerror(path):
+        raise ImportError
+
+    monkeypatch.setattr("importlib.import_module", mockerror)
+    
+    with pytest.raises(Exception):
+        get_class_attr("test", "notamodule")
---
+++ packages/mdo-engine/tests/user_plugins/definitions.py
@@ -0,0 +1,24 @@
+
+from mdo_engine.boundary import DataDefinition
+
+class testDefinition(DataDefinition):
+    
+    @property
+    def package_name(self):
+        
+        return "mdo_engine"
+        
+    @property
+    def company_name(self):
+        
+        return "DTOcean"
+    
+    @property
+    def local_yaml_dir(self):
+        '''The paths of the yaml definition files.'''
+        return None
+        
+    @property
+    def  user_yaml_dir(self):
+        '''The paths of the yaml definition files.'''
+        return "yaml"   
