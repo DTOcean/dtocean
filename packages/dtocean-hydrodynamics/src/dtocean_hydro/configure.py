066508e796e0b2576d84a8faa8d91ed5ab075857
Mathew Topper <damm_horse@yahoo.co.uk>
2025-01-27 11:10:08 +0000

    Add data installation routine

--- packages/dtocean-hydrodynamics/src/dtocean_hydro/configure.py
+++ packages/dtocean-hydrodynamics/src/dtocean_hydro/configure.py
@@ -16,12 +16,54 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging
-import os
+import shutil
+from platform import system
 
-from polite_config.configuration import ReadINI
-from polite_config.paths import EtcPath, SiteDataPath
+from polite_config.files import extract_tar, extract_zip
+from polite_config.paths import UserDataPath
 
 module_logger = logging.getLogger(__name__)
+
+_ARCH = system().lower() if system().lower() != "windows" else "win64"
+_VERSION_DATA = "v2025.01.0"
+_VERSION_NEMOH = "v2024.12.2"
+_DIR_DATA = UserDataPath("dtocean_data", "DTOcean", _VERSION_DATA)
+_DIR_NEMOH = UserDataPath("nemoh", "DTOcean", _VERSION_NEMOH)
+_URL_BASE_DATA = (
+    "https://github.com/DTOcean/dtocean-data-next/releases/"
+    f"download/{_VERSION_DATA}/dtocean-data-{_VERSION_DATA}"
+)
+_URL_BASE_NEMOH = (
+    f"https://github.com/DTOcean/NEMOH/releases/download/{_VERSION_NEMOH}/"
+    f"nemoh-{_ARCH}-{_VERSION_NEMOH}"
+)
+
+
+def get_data(force: bool = False):
+    arch = system().lower()
+
+    for url_base, dst in zip(
+        [_URL_BASE_DATA, _URL_BASE_NEMOH],
+        [_DIR_DATA, _DIR_NEMOH],
+    ):
+        if any(dst.iterdir()):
+            if not force:
+                raise RuntimeError(
+                    f"Directory {dst} is not empty. Consider setting 'force' "
+                    "argument to True"
+                )
+
+            shutil.rmtree(dst)
+
+        match arch:
+            case "linux":
+                url = url_base + ".tar.gz"
+                extract_tar(url, dst)
+            case "windows":
+                url = url_base + ".zip"
+                extract_zip(url, dst)
+            case _:
+                raise NotImplementedError("Unsupported architecture")
 
 
 def get_install_paths():
