9ccb806238679d08414152b8969b823b657d358d
Mathew Topper <damm_horse@yahoo.co.uk>
2025-01-29 17:10:26 +0000

    dtocean_tidal tests are passing locally

--- packages/dtocean-hydrodynamics/tests/dtocean_tidal/test_tidal_interface.py
+++ packages/dtocean-hydrodynamics/tests/dtocean_tidal/test_tidal_interface.py
@@ -21,7 +21,6 @@ Created on Fri Jun 02 12:37:42 2017
 .. moduleauthor:: Mathew Topper <damm_horse@yahoo.co.uk>
 """
 
-
 import numpy as np
 import numpy.ma as ma
 
@@ -29,13 +28,10 @@ from dtocean_tidal.interface import get_indices
 
 
 def test_get_indices():
-
     base = np.array([3, 5, 7, 1, 9, 8, 6, 6])
     search = np.array([2, 1, 5, 10, 100, 6])
-    
-    expected = ma.array([0, 3L, 1L, 0, 0, 6L],
-                        mask=[1, 0, 0, 1, 1, 0])
-    
+
     result = get_indices(search, base)
-    
+    expected = ma.array([0, 3, 1, 0, 0, 6], mask=[1, 0, 0, 1, 1, 0])
+
     assert (result == expected).all()
