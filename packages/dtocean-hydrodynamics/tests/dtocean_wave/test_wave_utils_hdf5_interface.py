214c6eb8611a20b8767e6180beab6a10fe30c017
Mathew Topper <damm_horse@yahoo.co.uk>
2025-02-05 11:57:39 +0000

    Convert main routine to test for hdf5_interface

---
+++ packages/dtocean-hydrodynamics/tests/dtocean_wave/test_wave_utils_hdf5_interface.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+from dtocean_wave.utils.hdf5_interface import (
+    load_dict_from_hdf5,
+    save_dict_to_hdf5,
+)
+
+
+def test_save_load(tmp_path):
+    data = {
+        "x": "astring",
+        "y": np.arange(10),
+        "d": {
+            "z": np.ones((2, 3)),
+            "b": b"bytestring",
+        },
+    }
+    filename = tmp_path / "test.h5"
+    save_dict_to_hdf5(data, filename)
+    dd = load_dict_from_hdf5(filename)
+
+    assert dd["x"].decode() == data["x"]
+    assert np.all(dd["y"] == data["y"])
+    assert np.all(dd["d"]["z"] == data["d"]["z"])
+    assert dd["d"]["b"] == data["d"]["b"]
