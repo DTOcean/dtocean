3614be9e6ac90480e6ff6e8a08e24d128005d212
Mathew Topper <damm_horse@yahoo.co.uk>
2025-02-06 17:34:39 +0000

    Test init of run nemoh project

---
+++ packages/dtocean-hydrodynamics/tests/dtocean_wec/projects/run_nemoh/conftest.py
@@ -0,0 +1,22 @@
+import pytest
+from PySide6.QtWidgets import QFileDialog
+
+
+@pytest.fixture
+def form_hyd(monkeypatch, qtbot, tmp_path, main_window):
+    monkeypatch.setattr(
+        QFileDialog,
+        "getExistingDirectory",
+        lambda *args: tmp_path,
+    )
+
+    main_window.entrance.btn_new.click()
+    main_window.new_project.btn_prj.click()
+    main_window.new_project.btn_t2.click()
+
+    def check_form():
+        assert main_window.form_hyd is not None
+
+    qtbot.waitUntil(check_form)
+
+    return main_window.form_hyd
