f407fab8434c3193ec6749551fb7c83cba27fdf5
Mathew Topper <damm_horse@yahoo.co.uk>
2025-02-07 18:18:58 +0000

    Add drag test (not working)

--- packages/dtocean-hydrodynamics/tests/dtocean_wec/projects/run_nemoh/test_run_nemoh_form_hyd.py
+++ packages/dtocean-hydrodynamics/tests/dtocean_wec/projects/run_nemoh/test_run_nemoh_form_hyd.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import pytest
+from PySide6.QtCore import QPoint, Qt
 
 from dtocean_wec.tab2 import QFileDialog
 from dtocean_wec.utils.mesh_plotter import PythonQtOpenGLMeshViewer
@@ -13,7 +14,7 @@ def test_form_hyd_load(form_hyd):
 
 
 @pytest.fixture
-def mesh_plotter(monkeypatch, form_hyd):
+def mesh_plotter(monkeypatch, form_hyd, main_window):
     mesh_path = THIS_DIR.parents[4] / "test_data" / "cube.GDF"
     monkeypatch.setattr(
         QFileDialog,
@@ -21,8 +22,23 @@ def mesh_plotter(monkeypatch, form_hyd):
         lambda *args: (str(mesh_path.absolute()), None),
     )
 
+    form_hyd.btn_mesh_f_t2.click()
     form_hyd.btn_view_mesh.click()
 
+    return main_window.mesh_view
 
-def test_open_mesh_plotter(mesh_plotter, main_window):
-    assert isinstance(main_window.mesh_view, PythonQtOpenGLMeshViewer)
+
+def test_open_mesh_plotter(mesh_plotter: PythonQtOpenGLMeshViewer):
+    assert isinstance(mesh_plotter, PythonQtOpenGLMeshViewer)
+
+
+def test_mesh_plotter_translate(qtbot, mesh_plotter: PythonQtOpenGLMeshViewer):
+    print(mesh_plotter.viewer3D.camera.position)
+
+    qtbot.mousePress(mesh_plotter, Qt.MouseButton.LeftButton)
+    qtbot.mouseMove(mesh_plotter, pos=QPoint(50, 0))
+    qtbot.mouseRelease(mesh_plotter, Qt.MouseButton.LeftButton)
+
+    print(mesh_plotter.viewer3D.camera.position)
+
+    assert False
