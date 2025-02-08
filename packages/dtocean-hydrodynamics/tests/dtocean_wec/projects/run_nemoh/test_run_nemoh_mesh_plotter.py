5dd8b516c9a6b03be643c9a7bacebe3c732fc4c8
Mathew Topper <damm_horse@yahoo.co.uk>
2025-02-08 15:29:07 +0000

    Add tests for mesh_plotter buttons

--- packages/dtocean-hydrodynamics/tests/dtocean_wec/projects/run_nemoh/test_run_nemoh_mesh_plotter.py
+++ packages/dtocean-hydrodynamics/tests/dtocean_wec/projects/run_nemoh/test_run_nemoh_mesh_plotter.py
@@ -6,7 +6,10 @@ from PySide6.QtCore import QPoint, Qt
 
 from dtocean_wec.tab2 import QFileDialog
 from dtocean_wec.utils.Camera import Camera
-from dtocean_wec.utils.mesh_plotter import PythonQtOpenGLMeshViewer
+from dtocean_wec.utils.mesh_plotter import (
+    PythonQtOpenGLMeshViewer,
+    Viewer3DWidget,
+)
 
 THIS_DIR = Path(__file__)
 
@@ -122,3 +125,99 @@ def test_mesh_plotter_zoom_wheel(
 
     assert not old_position == new_position
     mouse_zoom.assert_called_once()
+
+
+def test_mesh_plotter_structure_off(mocker, mesh_plotter):
+    drawMesh: MagicMock = mocker.spy(
+        Viewer3DWidget,
+        "drawMesh",
+    )
+    mesh_plotter.button1.click()
+
+    assert not mesh_plotter.viewer3D.drawMesh_cond
+    drawMesh.assert_not_called()
+
+
+def test_mesh_plotter_structure_on(qtbot, mocker, mesh_plotter):
+    drawMesh: MagicMock = mocker.spy(
+        Viewer3DWidget,
+        "drawMesh",
+    )
+    mesh_plotter.button1.click()
+    mesh_plotter.button1.click()
+    qtbot.waitUntil(lambda: drawMesh.assert_called())
+
+    args, _ = drawMesh.call_args
+    assert mesh_plotter.viewer3D.drawMesh_cond
+    assert len(args) == 1
+
+
+def test_mesh_plotter_wireframe_on(qtbot, mocker, mesh_plotter):
+    drawMesh: MagicMock = mocker.spy(
+        Viewer3DWidget,
+        "drawMesh",
+    )
+    mesh_plotter.button3.click()
+    qtbot.waitUntil(lambda: drawMesh.assert_called())
+
+    assert not mesh_plotter.viewer3D.drawMesh_cond
+    assert mesh_plotter.viewer3D.drawWire_cond
+
+    _, kwargs = drawMesh.call_args
+    assert len(kwargs) == 1
+    assert "wireframe" in kwargs
+    assert kwargs["wireframe"]
+
+
+def test_mesh_plotter_wireframe_off(qtbot, mocker, mesh_plotter):
+    bounding_box: MagicMock = mocker.spy(
+        Viewer3DWidget,
+        "bounding_box",
+    )
+    drawMesh: MagicMock = mocker.spy(
+        Viewer3DWidget,
+        "drawMesh",
+    )
+    mesh_plotter.button3.click()
+    qtbot.waitUntil(lambda: drawMesh.assert_called())
+
+    bounding_box.reset_mock()
+    drawMesh.reset_mock()
+    mesh_plotter.button3.click()
+    qtbot.waitUntil(lambda: bounding_box.assert_called())
+
+    assert not mesh_plotter.viewer3D.drawMesh_cond
+    assert not mesh_plotter.viewer3D.drawWire_cond
+    assert not drawMesh.called
+
+
+def test_mesh_plotter_norms_on(qtbot, mocker, mesh_plotter):
+    drawNorms: MagicMock = mocker.spy(
+        Viewer3DWidget,
+        "drawNorms",
+    )
+    mesh_plotter.button2.click()
+    qtbot.waitUntil(lambda: drawNorms.assert_called())
+
+    assert mesh_plotter.viewer3D.drawNorms_cond
+
+
+def test_mesh_plotter_norms_off(qtbot, mocker, mesh_plotter):
+    bounding_box: MagicMock = mocker.spy(
+        Viewer3DWidget,
+        "bounding_box",
+    )
+    drawNorms: MagicMock = mocker.spy(
+        Viewer3DWidget,
+        "drawNorms",
+    )
+    mesh_plotter.button2.click()
+    qtbot.waitUntil(lambda: drawNorms.assert_called())
+
+    bounding_box.reset_mock()
+    drawNorms.reset_mock()
+    mesh_plotter.button2.click()
+    qtbot.waitUntil(lambda: bounding_box.assert_called())
+
+    assert not mesh_plotter.viewer3D.drawNorms_cond
+    assert not drawNorms.called
