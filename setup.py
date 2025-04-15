2a036185eb9e46eef815a8f7b72fdbf2be657c74
Mathew Topper <damm_horse@yahoo.co.uk>
2025-04-15 12:15:57 +0100

    Remove redundant setup.py file

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='dtocean',
-      version='2022.07',
-      description='Meta package to record the DTOcean software version',
-      url='https://github.com/DTOcean/dtocean',
-      maintainer='Mathew Topper',
-      maintainer_email='mathew.topper@dataonlygreater.com',
-      license="GPLv3")
