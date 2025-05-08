354ba87eb0ce9c71e0dcd64387a371a393bf5571
Mathew Topper <damm_horse@yahoo.co.uk>
2025-05-08 13:12:17 +0100

    Reimplement GUIStrategyManager tests
    Move specific strategy tests for future use in other packages.

--- packages/dtocean-app/tests/test_strategies.py
+++ packages/dtocean-app/tests/test_strategies.py
@@ -69,3 +69,138 @@ def test_GUIAdvancedPosition_allow_run(mocker):
 def test_StrategyWidget_string2types(raw_string_input, expected):
     results = StrategyWidget.string2types(raw_string_input)
     assert results == expected
+
+
+@pytest.fixture
+def strategy_basic(mocker, qtbot, mock_shell):
+    mocker.patch.object(
+        GUIStrategyManager,
+        "get_available",
+        return_value=["Basic"],
+        autospec=True,
+    )
+
+    window = GUIStrategyManager(mock_shell)
+    window.show()
+    qtbot.addWidget(window)
+
+    if "Basic" not in window._plugin_names.keys():
+        pytest.skip("Test requires Basic strategy")
+
+    # Click on first strategy and apply
+    item = window.listWidget.item(0)
+    rect = window.listWidget.visualItemRect(item)
+    qtbot.mouseClick(
+        window.listWidget.viewport(), QtCore.Qt.LeftButton, pos=rect.center()
+    )
+
+    def apply_enabled():
+        assert window.applyButton.isEnabled()
+
+    qtbot.waitUntil(apply_enabled)
+
+    qtbot.mouseClick(window.applyButton, QtCore.Qt.LeftButton)
+
+    def strategy_set():
+        assert str(window.topDynamicLabel.text()) == str(item.text())
+
+    qtbot.waitUntil(strategy_set)
+
+    return window
+
+
+def test_strategy_basic_apply(strategy_basic):
+    assert str(strategy_basic.topDynamicLabel.text()) == "Basic"
+
+
+def test_strategy_basic_reset(qtbot, strategy_basic):
+    # Reset the strategy
+    qtbot.mouseClick(strategy_basic.resetButton, QtCore.Qt.LeftButton)
+
+    assert str(strategy_basic.topDynamicLabel.text()) == "None"
+
+
+@pytest.fixture
+def strategy_sensitivity(mocker, qtbot, mock_shell):
+    mocker.patch.object(
+        GUIStrategyManager,
+        "get_available",
+        return_value=["Unit Sensitivity"],
+        autospec=True,
+    )
+
+    window = GUIStrategyManager(mock_shell)
+    window.show()
+    qtbot.addWidget(window)
+
+    if "Unit Sensitivity" not in window._plugin_names.keys():
+        pytest.skip("Test requires Unit Sensitivity strategy")
+
+    widget_id = id(window.mainWidget)
+
+    # Click on first strategy and apply
+    item = window.listWidget.item(0)
+    rect = window.listWidget.visualItemRect(item)
+    qtbot.mouseClick(
+        window.listWidget.viewport(), QtCore.Qt.LeftButton, pos=rect.center()
+    )
+
+    def widget_changed():
+        assert id(window.mainWidget) != widget_id
+
+    qtbot.waitUntil(widget_changed)
+
+    return window
+
+
+def test_strategy_sensitivity_apply_null(strategy_sensitivity):
+    assert not strategy_sensitivity.applyButton.isEnabled()
+
+
+@pytest.fixture
+def strategy_sensitivity_variable(qtbot, strategy_sensitivity):
+    sensitivity = strategy_sensitivity.mainWidget
+
+    # Select module
+    idx = sensitivity.modBox.findText("Mock Module", QtCore.Qt.MatchFixedString)
+    sensitivity.modBox.setCurrentIndex(idx)
+
+    def varBox_not_empty():
+        assert int(sensitivity.varBox.count()) > 0
+
+    qtbot.waitUntil(varBox_not_empty)
+
+    # Select variable
+    idx = sensitivity.varBox.findText(
+        "Tidal Turbine Cut-In Velocity (m/s)", QtCore.Qt.MatchFixedString
+    )
+    sensitivity.varBox.setCurrentIndex(idx)
+
+    def lineEdit_enabled():
+        assert sensitivity.lineEdit.isEnabled()
+
+    qtbot.waitUntil(lineEdit_enabled)
+
+    return strategy_sensitivity
+
+
+def test_strategy_sensitivity_lineEdit_enabled(strategy_sensitivity_variable):
+    assert strategy_sensitivity_variable.mainWidget.lineEdit.isEnabled()
+
+
+def test_strategy_sensitivity_apply(strategy_sensitivity_variable):
+    line = strategy_sensitivity_variable.mainWidget.lineEdit
+    line.setText("1")
+
+    assert strategy_sensitivity_variable.applyButton.isEnabled()
+
+
+def test_strategy_sensitivity_apply_disable(strategy_sensitivity_variable):
+    line = strategy_sensitivity_variable.mainWidget.lineEdit
+    line.setText("1")
+
+    assert strategy_sensitivity_variable.applyButton.isEnabled()
+
+    line.setText("")
+
+    assert not strategy_sensitivity_variable.applyButton.isEnabled()
