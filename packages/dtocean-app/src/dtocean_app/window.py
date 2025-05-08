97cb1a22a49296c1fb61e32975e2973ca8faba19
Mathew Topper <damm_horse@yahoo.co.uk>
2025-05-08 13:03:24 +0100

    Remove pylint pragmas

--- packages/dtocean-app/src/dtocean_app/window.py
+++ packages/dtocean-app/src/dtocean_app/window.py
@@ -15,7 +15,6 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# pylint: disable=protected-access
 
 """
 Created on Thu Apr 23 12:51:14 2015
--- packages/dtocean-app/tests/definitions/test_definitions_simpledata.py
+++ packages/dtocean-app/tests/definitions/test_definitions_simpledata.py
@@ -15,7 +15,6 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# pylint: disable=redefined-outer-name,protected-access,unused-argument
 
 import pytest
 from attrdict import AttrDict
